c955930c2eb3d7316bd91f652ac15ddb466bfbbb
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-06-27 23:50:41 -0300

    adicionei os arquivos do spring boot

---
+++ src/main/java/sgcp/SgcpApplication.java
@@ -0,0 +1,13 @@
+package sgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sgcp/SgcpApplicationTests.java
@@ -0,0 +1,13 @@
+package sgcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
