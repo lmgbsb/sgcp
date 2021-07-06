9688581c364144e5b65062b74a4144e3f5e0c878
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-07-06 12:06:52 -0300

    atualize o arquivo application.properties para logar o SQL gerado pelo JPA e acrescentei a classe referente ao formulario de inclusao e alteracao de contrato

---
+++ src/main/java/sgcp/viewModel/contratoForm.java
@@ -0,0 +1,144 @@
+package sgcp.viewModel;
+
+public class contratoForm {
+
+	private String numero;
+	private String ano;
+	private String objeto;
+	private String diaAssinatura;
+	private String mesAssinatura;
+	private String anoAssinatura;
+	private String valor;
+	private String processoContratacao;
+	
+	private String diaInicioVigencia;
+	private String mesInicioVigencia;
+	private String anoInicioVigencia;
+	
+	private String diaFimVigengcia;
+	private String mesFimVigencia;
+	private String anoFimVigencia;
+	
+	private String matriculaFiscal;
+	
+	private String cpf1Preposto;
+	private String cpf2Preposto;
+	private String cpf3Preposto;
+	private String cpf4Preposto;
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getAno() {
+		return ano;
+	}
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	public String getObjeto() {
+		return objeto;
+	}
+	public void setObjeto(String objeto) {
+		this.objeto = objeto;
+	}
+	public String getDiaAssinatura() {
+		return diaAssinatura;
+	}
+	public void setDiaAssinatura(String diaAssinatura) {
+		this.diaAssinatura = diaAssinatura;
+	}
+	public String getMesAssinatura() {
+		return mesAssinatura;
+	}
+	public void setMesAssinatura(String mesAssinatura) {
+		this.mesAssinatura = mesAssinatura;
+	}
+	public String getAnoAssinatura() {
+		return anoAssinatura;
+	}
+	public void setAnoAssinatura(String anoAssinatura) {
+		this.anoAssinatura = anoAssinatura;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public String getProcessoContratacao() {
+		return processoContratacao;
+	}
+	public void setProcessoContratacao(String processoContratacao) {
+		this.processoContratacao = processoContratacao;
+	}
+	public String getDiaInicioVigencia() {
+		return diaInicioVigencia;
+	}
+	public void setDiaInicioVigencia(String diaInicioVigencia) {
+		this.diaInicioVigencia = diaInicioVigencia;
+	}
+	public String getMesInicioVigencia() {
+		return mesInicioVigencia;
+	}
+	public void setMesInicioVigencia(String mesInicioVigencia) {
+		this.mesInicioVigencia = mesInicioVigencia;
+	}
+	public String getAnoInicioVigencia() {
+		return anoInicioVigencia;
+	}
+	public void setAnoInicioVigencia(String anoInicioVigencia) {
+		this.anoInicioVigencia = anoInicioVigencia;
+	}
+	public String getDiaFimVigengcia() {
+		return diaFimVigengcia;
+	}
+	public void setDiaFimVigengcia(String diaFimVigengcia) {
+		this.diaFimVigengcia = diaFimVigengcia;
+	}
+	public String getMesFimVigencia() {
+		return mesFimVigencia;
+	}
+	public void setMesFimVigencia(String mesFimVigencia) {
+		this.mesFimVigencia = mesFimVigencia;
+	}
+	public String getAnoFimVigencia() {
+		return anoFimVigencia;
+	}
+	public void setAnoFimVigencia(String anoFimVigencia) {
+		this.anoFimVigencia = anoFimVigencia;
+	}
+	public String getMatriculaFiscal() {
+		return matriculaFiscal;
+	}
+	public void setMatriculaFiscal(String matriculaFiscal) {
+		this.matriculaFiscal = matriculaFiscal;
+	}
+	public String getCpf1Preposto() {
+		return cpf1Preposto;
+	}
+	public void setCpf1Preposto(String cpf1Preposto) {
+		this.cpf1Preposto = cpf1Preposto;
+	}
+	public String getCpf2Preposto() {
+		return cpf2Preposto;
+	}
+	public void setCpf2Preposto(String cpf2Preposto) {
+		this.cpf2Preposto = cpf2Preposto;
+	}
+	public String getCpf3Preposto() {
+		return cpf3Preposto;
+	}
+	public void setCpf3Preposto(String cpf3Preposto) {
+		this.cpf3Preposto = cpf3Preposto;
+	}
+	public String getCpf4Preposto() {
+		return cpf4Preposto;
+	}
+	public void setCpf4Preposto(String cpf4Preposto) {
+		this.cpf4Preposto = cpf4Preposto;
+	}
+	
+	
+}
